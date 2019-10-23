482a1a2cd13dbe01e64123395640405ae3979210
shenxin <shen.xin1@trans-cosmos.com.cn>
2019-10-23 13:23:09 +0800

    println 789

--- lianxi2019/src/lianxi2019/Add.java
+++ lianxi2019/src/lianxi2019/Add.java
@@ -8,14 +8,14 @@ public class Add {
 		// TODO Auto-generated method stub
 		int a;
 		int b;
-		Scanner scanner=new Scanner(System.in);
+		Scanner scanner = new Scanner(System.in);
 		System.out.println("请输入第一个加数");
-		a=new Integer(scanner.nextLine());
+		a = new Integer(scanner.nextLine());
 		System.out.println("请输入第二3个加数");
-		b=new Integer(scanner.nextLine());
-		int c=a+b;
-		System.out.println("a+b="+c);
-		System.out.println(456);
+		b = new Integer(scanner.nextLine());
+		int c = a + b;
+		System.out.println("a+b=" + c);
+		System.out.println(789);
 	}
 
 }
